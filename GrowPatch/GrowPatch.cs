1cc16027f45593c9d85f79e2fe68fb9a477d9eca
willis81808 <willis81808@gmail.com>
2023-01-20 22:28:46 -0700

    implemented transpiler

--- GrowPatch/GrowPatch.cs
+++ GrowPatch/GrowPatch.cs
@@ -2,8 +2,9 @@
 using UnityEngine; // requires UnityEngine.dll, UnityEngine.CoreModule.dll
 using HarmonyLib; // requires 0Harmony.dll
 using System;
-using Sonigon.Internal;
-using Sonigon;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
 
 // requires Assembly-CSharp.dll
 namespace GrowPatch
@@ -19,113 +20,106 @@ namespace GrowPatch
         {
             new Harmony(ModId).PatchAll();
         }
-        private void Start()
-        {
-        }
     }
     
-    [Serializable]
-    [HarmonyPatch(typeof(TrickShot), "Awake")]
+    [HarmonyPatch(typeof(TrickShot))]
     class TrickShotPatchAwake
     {
-		
-        private static bool Prefix(TrickShot __instance)
+        [HarmonyPrefix]
+		[HarmonyPatch("Awake")]
+        private static void AwakePrefix(TrickShot __instance)
         {
-			// replace this component with a fixed one
-			__instance.gameObject.AddComponent<FixedTrickShot>();
-			UnityEngine.GameObject.Destroy(__instance);
-
-			return false;
+            var data = __instance.gameObject.AddComponent<TrickShotData>();
         }
-	}
 
-	public class FixedTrickShot : MonoBehaviour
-	{
-		private void Awake()
-		{
-			this.trail = base.transform.root.GetComponentInChildren<ScaleTrailFromDamage>();
-		}
+        [HarmonyTranspiler]
+        [HarmonyPatch("Update")]
+        static IEnumerable<CodeInstruction> UpdateTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var code = new List<CodeInstruction>(instructions);
 
-		private void Start()
-		{
-			this.projectileHit = base.GetComponentInParent<ProjectileHit>();
-			this.move = base.GetComponentInParent<MoveTransform>();
-			if (this.projectileHit != null)
-			{
-				if (this.soundGrowExplosion != null)
-				{
-					this.projectileHit.AddHitActionWithData(new Action<HitInfo>(this.SoundPlayGrowExplosion));
-				}
-				if (this.soundGrowWail != null)
-				{
-					this.soundGrowWailPlayed = true;
-					SoundManager.Instance.Play(this.soundGrowWail, this.projectileHit.ownPlayer.transform);
-				}
-			}
-		}
+            FieldInfo projectileHitField = AccessTools.Field(typeof(TrickShot), "projectileHit");
+            FieldInfo projectileHitShakeField = AccessTools.Field(typeof(ProjectileHit), "shake");
 
-		public void SoundPlayGrowExplosion(HitInfo hit)
-		{
-			if (!this.soundGrowExplosionPlayed)
-			{
-				this.soundGrowExplosionPlayed = true;
-				if (this.soundGrowExplosion != null)
-				{
-					SoundManager.Instance.PlayAtPosition(this.soundGrowExplosion, this.projectileHit.ownPlayer.transform, hit.point, new SoundParameterBase[]
-					{
-					this.soundIntensity
-					});
-				}
-				if (this.soundGrowWailPlayed)
-				{
-					SoundManager.Instance.Stop(this.soundGrowWail, this.projectileHit.ownPlayer.transform, true);
-				}
-			}
-		}
-		// replaces Update from the vanilla game with FixedUpdate
-		private void FixedUpdate()
-		{
-			if (this.move.distanceTravelled > this.removeAt)
-			{
-				UnityEngine.GameObject.Destroy(this);
-				return;
-			}
-			this.soundIntensity.intensity = this.move.distanceTravelled / this.removeAt;
-			float num = this.move.distanceTravelled - this.lastDistanceTravelled;
-			this.lastDistanceTravelled = this.move.distanceTravelled;
-			// replaces TimeHandler.deltaTime with TimeHandler.fixedDeltaTime
-			float num2 = 1f + num * TimeHandler.fixedDeltaTime * base.transform.localScale.x * this.muiltiplier;
-			this.projectileHit.damage *= num2;
-			this.projectileHit.shake *= num2;
-			if (this.trail)
-			{
-				this.trail.Rescale();
-			}
-		}
+            int startIndex = -1;
+            int endIndex = -1;
+            for (int i = 0; i < code.Count; i++)
+            {
+                var currentInstruction = code[i];
 
-		[Header("Sound")]
-		public SoundEvent soundGrowExplosion;
+                // /* 0x0002CF8E */ IL_0082: ldfld     class ProjectileHit TrickShot::projectileHit	// Finds the value of a field in the object whose reference is currently on the evaluation stack.
+                if (startIndex < 0 && currentInstruction.opcode == OpCodes.Ldfld && currentInstruction.LoadsField(projectileHitField))
+                    startIndex = i;
 
-		public SoundEvent soundGrowWail;
+                // /* 0x0002CFAE */ IL_00A2: stfld     float32 ProjectileHit::shake	// Replaces the value stored in the field of an object reference or pointer with a new value.
+                if (endIndex < 0 && currentInstruction.opcode == OpCodes.Stfld && currentInstruction.StoresField(projectileHitShakeField))
+                    endIndex = i;
+            }
 
-		private bool soundGrowExplosionPlayed;
+            if (startIndex < 0 || endIndex < 0)
+            {
+                UnityEngine.Debug.LogError($"[TrickShot] Update transpiler unable to find code block to replace");
+                return code;
+            }
 
-		private bool soundGrowWailPlayed;
+            code.RemoveRange(startIndex, (endIndex - startIndex) + 1);
+            code.InsertRange(startIndex, new List<CodeInstruction>
+            {
+                // TrickShotData.ApplyScaling(base.gameObject);
+                CodeInstruction.Call(typeof(UnityEngine.Component), "get_gameObject"),
+                CodeInstruction.Call(typeof(TrickShotData), nameof(TrickShotData.ApplyScaling), parameters: new []{ typeof(GameObject) })
+            });
 
-		private SoundParameterIntensity soundIntensity = new SoundParameterIntensity(0f, UpdateMode.Once);
+            return code;
+        }
+    }
 
-		[Header("Settings")]
-		public float muiltiplier = 1f;
+    class TrickShotData : MonoBehaviour
+    {
+        public float baseDamage, baseShake, timeCounter, distanceCounter;
+        
+        private ProjectileHit projectileHit;
+        private ScaleTrailFromDamage trail;
 
-		public float removeAt = 30f;
+        private Vector3 lastPos;
 
-		private ProjectileHit projectileHit;
+        void Awake()
+        {
+            projectileHit = GetComponentInParent<ProjectileHit>();
+            trail = transform.root.GetComponentInChildren<ScaleTrailFromDamage>();
+        }
 
-		private MoveTransform move;
+        void Start()
+        {
+            baseDamage = projectileHit.damage;
+            baseShake = projectileHit.shake;
+            timeCounter = 0;
+            distanceCounter = 0;
+            lastPos = transform.position;
+        }
 
-		private ScaleTrailFromDamage trail;
+        void Update()
+        {
+            timeCounter += TimeHandler.deltaTime;
+            distanceCounter += Vector3.Distance(transform.position, lastPos);
+            lastPos = transform.position;
+        }
 
-		private float lastDistanceTravelled;
-	}
+        private void Apply()
+        {
+            //var factor = 1/Mathf.Pow(0.1f, timeCounter) + 1/Mathf.Pow(0.9f, distanceCounter);
+            //projectileHit.damage = (baseDamage + factor) * (1 + (factor / 55f));
+            ///projectileHit.shake = baseShake + factor;
 
+            var factor = Mathf.Sqrt(timeCounter) * 25f + Mathf.Sqrt(distanceCounter) * 10f;
+            projectileHit.damage = baseDamage + (baseDamage * (factor / 55f));
+
+            UnityEngine.Debug.Log($"Time: {timeCounter}\nDistance: {distanceCounter}\nFactor: {factor}\n\n");
+        }
+
+        public static void ApplyScaling(GameObject projectile)
+        {
+            projectile.GetComponent<TrickShotData>().Apply();
+        }
+    }
 }
